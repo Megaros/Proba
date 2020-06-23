ebb2f60c5afd2eaf9604722106fbaa9fada14b16
Megaros <megaros22@gmail.com>
2020-06-23 20:52:30 +0300

    The first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('Олег');
+
+
+console.log(4 / 0);
+let samfing
+let persone = {
+    name: "Oleg",
+    age: 25,
+
+}
+console.log(persone.name,
+     persone.age
+);
+console.log(samfing);
+let arr = ['plam.png', 'orang.jpg', 'apple.bmp']
+console.log(arr[2])
+console.log('https://vk.com/catalog/' + 5)
+console.log(typeof(+'5'))
+
+console.log(5%3)
+// let ans = prompt('ПРИВЕТ', 'Олег')
+// console.log(ans)
+// let sw = null
+// let answ = confirm('msg');
+// console.log(answ)
+
